919d9f078d864d80fb7bf0e44f30f1a248f12b96
Ruben de Vries <ruben@rubensayshi.com>
2021-08-19 11:54:14 +0200

    exclude tags and remotes. prioritize master. support multiple dirs to pack

--- githooks/entpain-post-checkout.sh
+++ githooks/entpain-post-checkout.sh
@@ -5,7 +5,8 @@ set -e
 DEBUG=false
 ROOTDIR=$(pwd)
 ENTPAINDIR=${ROOTDIR}/tmp/entpain
-
+ENTPAINPACKDIRS=${ENTPAINPACKDIRS:-ent/}
+ENTPAINPACKDIRS=$(echo "$ENTPAINPACKDIRS" | tr "," "\n")
 # echo "post-checkout hook in ${ROOTDIR}"
 
 PREVHEAD=$1
@@ -22,8 +23,15 @@ function entpainpack() {
 
   mkdir -p $ENTPAINDIR
 
-  # make a list of files which are on gitignore
-  git clean -n -d -X ent/ | sed 's/Would remove //g' > ${ENTPAINDIR}/$BRANCHSLUG.files
+  # reset list of files to pack
+  echo "" > ${ENTPAINDIR}/$BRANCHSLUG.files
+
+  # for each dir to pack add to the list of files
+  for ENTPAINPACKDIR in $ENTPAINPACKDIRS; do
+    # make a list of files which are on gitignore
+    git clean -n -d -X $ENTPAINPACKDIR | sed 's/Would remove //g' >> ${ENTPAINDIR}/$BRANCHSLUG.files
+  done
+
   # create an archive of all those files, so we can switch back to it later
   tar czf ${ENTPAINDIR}/$BRANCHSLUG.tar.gz -T ${ENTPAINDIR}/$BRANCHSLUG.files
 }
@@ -36,27 +44,37 @@ function entpainunpack() {
   if [[ -f ${ENTPAINDIR}/${BRANCHSLUG}.tar.gz ]]; then
     echo "entpain:unpack $BRANCH"
 
-    # delete any git ignored files
-    git clean -q -f -d -X ent/
+    # for each dir to unpack delete any git ignored files
+    for ENTPAINPACKDIR in $ENTPAINPACKDIRS; do
+      git clean -q -f -d -X $ENTPAINPACKDIR
+    done
     # unpack from our archive
     tar xzf ${ENTPAINDIR}/${BRANCHSLUG}.tar.gz
   fi
 }
 
 if [[ $CHECKOUTTYPE == "branch" ]]; then
-  PREVBRANCH=$(git name-rev --name-only $PREVHEAD || echo "")
-  NEWBRANCH=$(git name-rev --name-only $NEWHEAD || echo "")
+  PREVBRANCH=$(git name-rev --exclude 'remotes/*' --exclude='tags/*' --name-only $PREVHEAD || echo "")
+  NEWBRANCH=$(git name-rev --exclude 'remotes/*' --exclude='tags/*' --name-only $NEWHEAD || echo "")
+
+  # master is special, there's often other branches with the same ref but we want to prioritize master ...
+  MASTERSHA=$(git rev-parse master)
+  if [[ "$PREVHEAD" == "$MASTERSHA" ]]; then
+    PREVBRANCH=master
+  fi
+  if [[ "$NEWHEAD" == "$MASTERSHA" ]]; then
+    NEWBRANCH=master
+  fi
 
   if [[ "$PREVBRANCH" != "$NEWBRANCH" ]]; then
     [[ "${DEBUG}" == "true" ]] && \
       echo "entpain: ${PREVHEAD} (${PREVBRANCH}) -> ${NEWHEAD} (${NEWBRANCH})"
-
     # if we know which branch we're switch away from then we can create an archive for it
-    if [[ "$PREVBRANCH" != "" && ! "$PREVBRANCH" =~ remotes\/.+ && ! "$PREVBRANCH" =~ ~[0-9]+$ ]]; then
+    if [[ "$PREVBRANCH" != "" && ! "$PREVBRANCH" =~ remotes\/.+ && ! "$PREVBRANCH" =~ ~[0-9]+$ && "$PREVBRANCH" != "undefined" ]]; then
       entpainpack "$PREVBRANCH"
     fi
     # if we know which branch we're switch towards than we can try to unpack its archive
-    if [[ "$NEWBRANCH" != "" && ! "$NEWBRANCH" =~ remotes\/.+ && ! "$NEWBRANCH" =~ ~[0-9]+$  ]]; then
+    if [[ "$NEWBRANCH" != "" && ! "$NEWBRANCH" =~ remotes\/.+ && ! "$NEWBRANCH" =~ ~[0-9]+$ && "$NEWBRANCH" != "undefined" ]]; then
       entpainunpack "$NEWBRANCH"
     fi
   fi
