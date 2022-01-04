819953d8c13e246c6a95ca24fa4253b2229dd26e
Ruben de Vries <ruben@rubensayshi.com>
2022-01-04 09:51:21 +0100

    add entpain post-rewrite hook

---
+++ entpain-git-hook-post-rewrite.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -e
+
+DEBUG=false
+ROOTDIR=$(pwd)
+ENTPAINDIR=${ROOTDIR}/tmp/entpain
+ENTPAINPACKDIRS=${ENTPAINPACKDIRS:-ent/,quickfix/dtcc/,}
+ENTPAINPACKDIRS=$(echo "$ENTPAINPACKDIRS" | tr "," "\n")
+
+function dbg() {
+  echo "$*" | tee -a ${ROOTDIR}/tmp/entpain.log 1>&2
+}
+
+function entpainunpack() {
+  BRANCH=$1
+  BRANCHSLUG=$(echo "$1" | sed 's/\//__/g')
+
+  # can only unpack if there's an archive
+  if [[ -f ${ENTPAINDIR}/${BRANCHSLUG}.tar.gz ]]; then
+    dbg "entpain:unpack $BRANCH"
+
+    # for each dir to unpack delete any git ignored files
+    for ENTPAINPACKDIR in $ENTPAINPACKDIRS; do
+      if [[ -d $ENTPAINPACKDIR ]]; then
+        git clean -q -f -d -X $ENTPAINPACKDIR
+      fi
+    done
+    # unpack from our archive
+    tar xzf ${ENTPAINDIR}/${BRANCHSLUG}.tar.gz
+  fi
+}
+
+
+if [[ "$1" == "rebase" && "$2" == "" ]]; then
+  HEAD=
+  while read LINE
+  do
+    if [[ "$LINE" != "" ]]; then
+      COMMITS=($LINE)
+      HEAD=${COMMITS[1]}
+    fi
+  done < /dev/stdin
+
+  BRANCH=$(git name-rev --exclude 'remotes/*' --exclude='tags/*' --name-only $HEAD || echo "")
+  # master is special, there's often other branches with the same ref but we want to prioritize master ...
+  MASTERSHA=$(git rev-parse master)
+  if [[ "$VHEAD" == "$MASTERSHA" ]]; then
+    BRANCH=master
+  fi
+
+  dbg "entpain:post-rewrite $HEAD = $BRANCH"
+
+  # if we know which branch we're switch towards than we can try to unpack its archive
+  if [[ "$BRANCH" != "" && ! "$BRANCH" =~ remotes\/.+ && ! "$BRANCH" =~ ~[0-9]+$ && "$BRANCH" != "undefined" ]]; then
+    entpainunpack "$BRANCH"
+  fi
+fi
